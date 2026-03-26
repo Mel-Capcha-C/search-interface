f77fc8ff46d5b98a819d3739e916998247440eb3
Mel-Capcha-C <mel.capcha.c@uni.pe>
2026-03-26 11:36:20 -0500

    Tab Widget and Combo Box Update 25th March

--- my_library.py
+++ my_library.py
@@ -1,8 +1,11 @@
 from PySide6.QtWidgets import (
+    QAbstractItemView,
+    QButtonGroup,
     QGridLayout,
     QGroupBox,
     QSizePolicy,
     QStatusBar,
+    QTabWidget,
     QWidget,
     QMainWindow,
     QPushButton,
@@ -14,6 +17,8 @@ from PySide6.QtWidgets import (
     QTextEdit,
     QCheckBox,
     QRadioButton,
+    QListWidget,
+    QComboBox,
 )
 from PySide6.QtCore import QSize
 from PySide6.QtGui import QAction, QIcon, QPixmap
@@ -204,14 +209,18 @@ class rockMainWindow(QMainWindow):
         os.setLayout(os_layout)
         os.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
 
-        os.exclusive = False
         # Radio buttons
         answer_group = QGroupBox("Choose an answer")
         answer_a = QRadioButton("Answer a")
         answer_b = QRadioButton("Answer b")
         answer_c = QRadioButton("Answer c")
 
-        answer_group.exclusive = True  # Only one radio button can be selected at a time
+        exclusive_button_group = (
+            QButtonGroup()
+        )  # Only one radio button can be selected at a time
+        exclusive_button_group.addButton(answer_a)
+        exclusive_button_group.addButton(answer_b)
+        exclusive_button_group.addButton(answer_c)
 
         answer_layout = QVBoxLayout()
         answer_layout.addWidget(answer_a)
@@ -220,8 +229,88 @@ class rockMainWindow(QMainWindow):
 
         answer_group.setLayout(answer_layout)
 
-        # Primary Layout
+        # List Widget
+        self.list_widget = QListWidget()
+        self.list_widget.setSelectionMode(
+            QAbstractItemView.SelectionMode.MultiSelection
+        )
+        self.list_widget.addItem("Uno")
+        self.list_widget.addItems(["Dos", "Tres"])
 
+        self.list_widget.currentItemChanged.connect(self.lw_current_item_changed)
+        self.list_widget.currentTextChanged.connect(self.lw_current_text_changed)
+
+        lw_button_add_item = QPushButton("Add Item")
+        lw_button_add_item.clicked.connect(self.lw_add_item)
+
+        lw_button_delete_item = QPushButton("Delete Item")
+        lw_button_delete_item.clicked.connect(self.lw_delete_item)
+
+        lw_button_item_count = QPushButton("Item Count")
+        lw_button_item_count.clicked.connect(self.lw_item_count)
+
+        lw_button_selected_items = QPushButton("Selected Items")
+        lw_button_selected_items.clicked.connect(self.lw_selected_items)
+
+        lw_button_layout = QVBoxLayout()
+        lw_button_layout.addWidget(lw_button_add_item)
+        lw_button_layout.addWidget(lw_button_delete_item)
+        lw_button_layout.addWidget(lw_button_item_count)
+        lw_button_layout.addWidget(lw_button_selected_items)
+
+        lw_layout = QHBoxLayout()
+        lw_layout.addWidget(self.list_widget, 2)
+        lw_layout.addLayout(lw_button_layout, 1)
+
+        lw_widget = QWidget()
+        lw_widget.setLayout(lw_layout)
+
+        # Tab Widget
+        tab_widget = QTabWidget()
+        first_tab = QWidget()
+        second_tab = QWidget()
+
+        tw_label = QLabel("This is the second tab")
+        tw_button1 = QPushButton("Button 1")
+        tw_button1.setCheckable(True)
+        tw_button1.clicked.connect(
+            lambda checked: print(f"Button 1 in second tab clicked: {checked}")
+        )
+
+        # Combo Box
+        self.combo_box = QComboBox(self)
+        cb_label = QLabel("Choose a planet:")
+        # Add planets
+        self.combo_box.addItems(["Earth", "Venus", "Mars", "Pluton", "Jupiter"])
+
+        cb_button_widget = QWidget()
+
+        cb_button_current_value = QPushButton("Current value")
+        # cb_button_current_value.clicked.connect(self.cb_current_value)
+        cb_button_set_current = QPushButton("Set value")
+        # cb_button_set_current.clicked.connect(self.cb_set_current)
+        cb_button_get_values = QPushButton("Get values")
+        # cb_button_get_values.clicked.connect(self.cb_get_values)
+
+        cb_button_layout = QHBoxLayout()
+        cb_button_layout.addWidget(cb_button_current_value)
+        cb_button_layout.addWidget(cb_button_set_current)
+        cb_button_layout.addWidget(cb_button_get_values)
+
+        cb_button_widget.setLayout(cb_button_layout)
+
+        # Second tab layout
+        tw_second_layout = QVBoxLayout()
+        tw_second_layout.addWidget(tw_label)
+        tw_second_layout.addWidget(tw_button1)
+        tw_second_layout.addSpacing(20)
+        tw_second_layout.addWidget(cb_label)
+        tw_second_layout.addWidget(self.combo_box)
+        tw_second_layout.addWidget(cb_button_widget)
+
+        second_tab.setLayout(tw_second_layout)
+
+        # Primary Layout
         primary_layout = QVBoxLayout()
         primary_layout.addLayout(le_layout)
         primary_layout.addSpacing(20)
@@ -240,11 +329,17 @@ class rockMainWindow(QMainWindow):
         secondary_layout.addWidget(os)
         secondary_layout.addSpacing(20)
         secondary_layout.addWidget(answer_group)
+        secondary_layout.addSpacing(20)
+        secondary_layout.addWidget(lw_widget)
+
+        first_tab.setLayout(secondary_layout)
+        tab_widget.addTab(first_tab, "First Tab")
+        tab_widget.addTab(second_tab, "Second Tab")
 
         main_layout = QHBoxLayout()
         main_layout.addWidget(primary_widget)
         main_layout.addSpacing(20)
-        main_layout.addLayout(secondary_layout)
+        main_layout.addWidget(tab_widget)
 
         central_widget = QWidget()
         central_widget.setLayout(main_layout)
@@ -329,3 +424,29 @@ class rockMainWindow(QMainWindow):
     def te_current_text_button_clicked(self):
         current_text = self.text_edit.toPlainText()
         print("Current text in QTextEdit:", current_text)
+
+    def lw_current_item_changed(self, item):
+        print("Current item:", item.text())
+
+    def lw_current_text_changed(self, text):
+        print("Current text changed:", text)
+
+    def lw_add_item(self):
+        new_item_text = f"Item {self.list_widget.count() + 1}"
+        self.list_widget.addItem(new_item_text)
+
+    def lw_delete_item(self):
+        # self.list_widget.takeItem(self.list_widget.currentRow())
+        selected_items = self.list_widget.selectedItems()
+        for item in selected_items:
+            print(f"Deleting item: {item.text()}")
+            self.list_widget.takeItem(self.list_widget.row(item))
+
+    def lw_item_count(self):
+        count = self.list_widget.count()
+        print(f"Item count: {count}")
+
+    def lw_selected_items(self):
+        selected_items = self.list_widget.selectedItems()
+        selected_texts = [item.text() for item in selected_items]
+        print("Selected items:", selected_texts)
